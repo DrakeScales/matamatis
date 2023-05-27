90ee7e015117a8adda231082ee2cfba2377798f3
DrakeScales <110292679+DrakeScales@users.noreply.github.com>
2023-05-27 12:09:40 +0530

    Add files via upload

---
+++ quizlogin.js
@@ -0,0 +1,9 @@
+function adduser(){
+    var player1 = document.getElementById("username1").value;
+    var player2 = document.getElementById("username2").value;
+
+    localStorage.setItem("player1",player1);
+    localStorage.setItem("player2",player2);
+
+    window.location="quizpage.html";
+}
